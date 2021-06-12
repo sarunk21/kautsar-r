4467ecc5dfb7ee765786d90b772dd2383324594e
kautsar_r <kautsarmkp@gmail.com>
2021-06-12 10:41:46 +0700

    Add Navbar and font

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="container px-8 mx-auto"></div>
+  <div class="container px-8 mx-auto">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: { Navbar },
+};
+</script>
+
+<style>
+#app {
+  @apply font-poppins;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="grid grid-cols-12 justify-between items-center py-4">
+    <img
+      src="@/assets/logo.png"
+      class="rounded-full col-span-6 w-16"
+      alt="Logo Sarunk"
+    />
+    <button class="p-4 ml-auto col-span-6 md:hidden" @click="open = !open">
+      <i class="fa fa-bars"></i>
+    </button>
+    <ul class="link" :class="[{ hidden: !open }, { flex: open }]">
+      <li><a href="#">About</a></li>
+      <li><a href="#">Project</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+li {
+  @apply mr-3 md:mr-0 md:ml-6 border-b-0;
+}
+
+li:hover {
+  border-color: #4eb18d;
+  @apply border-b-2;
+}
+
+li a {
+  color: #4eb18d;
+  @apply text-lg font-semibold md:text-xl;
+}
+
+.link {
+  @apply col-span-12 justify-start items-center mt-5 md:mt-0 md:justify-end md:col-span-6 md:flex;
+}
+</style>
