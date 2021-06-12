3fb7d492f150fbbc0898489a34faa7d9a54f6667
kautsar_r <kautsarmkp@gmail.com>
2021-06-12 10:42:47 +0700

    Add Hero

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container px-8 mx-auto">
     <Navbar />
+    <Hero />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
 
 export default {
-  components: { Navbar },
+  components: { Navbar, Hero },
 };
 </script>
 
---
+++ src/components/Hero.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="grid grid-cols-12 justify-between md:my-14">
+    <div class="col-span-12 md:col-span-6 mb-12">
+      <div class="mt-6">
+        <h1 class="font-semibold color-green text-3xl md:text-5xl mb-4 md:mb-8">
+          Selamat Datang
+        </h1>
+        <span class="color-brown text-2xl md:text-4xl">
+          <span id="title"></span> Code
+        </span>
+      </div>
+      <div class="mt-6 md:mt-12">
+        <a href="" class="btn-about">About Me</a>
+        <a href="" class="btn-project ml-6">My Project</a>
+      </div>
+    </div>
+    <div class="col-span-12 md:col-span-6 order-1">
+      <img src="@/assets/hero.svg" alt="Hero Image" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Typed from "typed.js";
+
+export default {
+  mounted() {
+    const title = document.getElementById("title");
+    let type = new Typed(title, {
+      strings: ["Create ^1000", "Read ^1200", "Update ^1400", "Delete ^1600"],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    });
+  },
+};
+</script>
+
+<style scoped>
+.color-dark {
+  color: #211f20;
+}
+
+.color-green {
+  color: #4eb18d;
+}
+
+.color-brown {
+  color: #4b2c2f;
+}
+
+.btn {
+  @apply px-4 py-2 border rounded-lg font-medium duration-200;
+}
+
+.btn-about {
+  background-color: #4eb18d;
+  color: white;
+  @apply btn;
+}
+
+.btn-about:hover {
+  background-color: #67dbb1;
+}
+
+.btn-project {
+  border-color: #4eb18d;
+  color: #4eb18d;
+  @apply btn;
+}
+
+.btn-project:hover {
+  border-color: #67dbb1;
+  color: #67dbb1;
+}
+</style>
